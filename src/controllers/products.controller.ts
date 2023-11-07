800e471f1dd8d987de211c5e9253530c2c9a4d4a
Moriones20 <moriones06082001@gmail.com>
2023-11-07 17:26:21 -0500

    implement services

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -10,44 +10,39 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { ProductsService } from 'src/services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products limit => ${limit}, offset => ${offset}, brand => ${brand}`;
+    return this.productService.findAll(limit, offset, brand);
   }
 
-  @Get(':productId')
+  @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Param('productId') productiId: string) {
-    return {
-      message: `product #${productiId}`,
-    };
+  getProduct(@Param('id') id: string) {
+    return this.productService.findById(+id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'create action',
-      data: payload,
-    };
+    return this.productService.create(payload);
   }
 
   @Put(':id')
-  update(@Param(':id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(@Param('id') id: string, @Body() payload: any) {
+    return this.productService.update(+id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return id;
+  delete(@Param('id') id: string) {
+    return this.productService.delete(+id);
   }
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -15,21 +15,48 @@ export class ProductsService {
     },
   ];
 
-  findAll() {
-    return this.products;
+  findAll(limit: number, offset: number, brand: string) {
+    return {
+      limit,
+      offset,
+      brand,
+      data: this.products,
+    };
   }
 
-  findOne(id: number) {
+  findById(id: number) {
     return this.products.find((item) => item.id === id);
   }
 
   create(payload: any) {
-    this.counterId = this.counterId++;
+    this.counterId = this.counterId + 1;
     const newProduct = {
       id: this.counterId,
       ...payload,
     };
     this.products.push(newProduct);
     return newProduct;
+  }
+
+  update(id: number, payload: any) {
+    const product = this.findById(id);
+    if (product) {
+      const index = this.products.findIndex((item) => item.id === id);
+      this.products[index] = {
+        ...product,
+        ...payload,
+      };
+      return this.products[index];
+    }
+    return null;
+  }
+
+  delete(id: number) {
+    const product = this.findById(id);
+    if (product) {
+      const index = this.products.findIndex((item) => item.id === id);
+      delete this.products[index];
+    }
+    return null;
   }
 }
