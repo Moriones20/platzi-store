3e69c9c30eab86315220f86d6c817c400e77ece4
Moriones20 <moriones06082001@gmail.com>
2023-11-07 15:53:50 -0500

    response status code

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
@@ -12,17 +14,20 @@ import {
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProduct(@Param('productId') productiId: string) {
-    return `product #${productiId}`;
-  }
-
-  @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
     return `products limit => ${limit}, offset => ${offset}, brand => ${brand}`;
+  }
+
+  @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getProduct(@Param('productId') productiId: string) {
+    return {
+      message: `product #${productiId}`,
+    };
   }
 
   @Post()
