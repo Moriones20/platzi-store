76ef57d030d217c311eda66e9c234e7972c977f9
Moriones20 <moriones06082001@gmail.com>
2023-11-10 16:22:30 -0500

    Global Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,13 @@ import { ProductsModule } from '@/modules/products/products.module';
 import { UsersModule } from '@/modules/users/users.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
-
-const API_KEY = 'DEV';
-const API_KEY_PROD = 'PROD';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [HttpModule, ProductsModule, UsersModule],
+  imports: [HttpModule, ProductsModule, UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'DATA',
       useFactory: async (http: HttpService) => {
