21a674cdd2311dbf9392e7298185ee16bdeda5b6
Moriones20 <moriones06082001@gmail.com>
2023-11-20 17:38:47 -0500

    Setting up Typeorm Module

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,11 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return {
     database: {
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
       name: process.env.DATABASE_NAME,
-      port: process.env.DATABASE_PORT,
     },
     apiKey: process.env.API_KEY,
   };
